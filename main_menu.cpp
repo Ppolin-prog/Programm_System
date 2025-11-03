7ab7c6ce87db71ad31bb681b45b5de065e586c22
Purpururi <ppoluska@gmail.com>
2025-11-03 22:47:44 +0300

    Changed_main_menu

--- main_menu.cpp
+++ main_menu.cpp
@@ -1,13 +1,17 @@
 ﻿# include <iostream>
 # include "check.h"
 # include "choice_menu.h"
+#define NOMINMAX
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
 using namespace std;
 
 enum Menu { Start = 1, End};
 
 int main() {
-	setlocale(LC_ALL, "Russian");
-	cout << "Поразрядная сортировка, версия MSJ" << endl;
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	cout << "Поразрядная сортировка, версия MSD" << endl;
 	int choice = 0;
 	do {
 		cout << "Пункты меню: " << endl << "Начать работу - 1" << endl << "Выйти - 2" << endl;
@@ -17,13 +21,13 @@ int main() {
 		case Start: 
 			cout << "Начало работы программы" << endl;
 			Implement_Sort();
-			return 0;
+			break;
 		case End: 
 			cout << "Окончание работы программы" << endl;
 			return 0;
 		default: // 6(-О-)9
 			cout << "Некоректный ввод. Пункт отсутствует в меню." << endl;
-			return 0;
+			break;
 		}
 	} while (true);
 	return 0;
